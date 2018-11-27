6b3aa8cdf750c9bda50d48facf81a2c3fe44209b
Antonia Elek <antoniaelek@hotmail.com>
2018-11-27 22:34:30 +0100

    Parse quotes from all pages.

--- src/goodreads.py
+++ src/goodreads.py
@@ -3,18 +3,37 @@ import html
 
 
 def get_quotes(user_id, user_name, sort=False):
-    feed = feedparser.parse('https://www.goodreads.com/quotes/list_rss/' + user_id + '-' + user_name)
-
+    url = 'https://www.goodreads.com/quotes/list_rss/' + user_id + '-' + user_name
     quotes = []
-
-    for entry in feed.entries:
-        summary = entry.summary
-        sep = summary.rfind('-- ')
-        quote = html.unescape(summary[1:sep-2])
-        author = html.unescape(summary[sep+3:])
-        quotes.append((author, quote))
+    page = 1
+    while True:
+        try:
+            new_quotes = _parse_rss_page(url + '?page=' + str(page))
+            if len(new_quotes) == 0:
+                break
+            quotes.extend(new_quotes)
+            page = page + 1
+        except:
+            break
 
     if sort:
         quotes.sort()
 
     return quotes
+
+
+def _parse_rss_page(url):
+    feed = feedparser.parse(url)
+    page_quotes = []
+
+    for entry in feed.entries:
+        try:
+            summary = entry.summary
+            sep = summary.rfind('-- ')
+            quote = html.unescape(summary[1:sep - 2])
+            author = html.unescape(summary[sep + 3:])
+            page_quotes.append((author, quote))
+        except:
+            continue
+
+    return page_quotes
--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from functions import generate_image
 from goodreads import get_quotes
 
-out_folder = "data/out/"
+out_folder = "data/out/antonia/"
 
 if __name__ == '__main__':
-    quotes = get_quotes('64367923', 'anamaria-elek', sort=True)
+    quotes = get_quotes('5705326', 'antonia', sort=True)
 
-    quote = quotes[4]
-    generate_image(out_folder, quote[1], quote[0])
+    # quote = quotes[4]
+    # generate_image(out_folder, quote[1], quote[0])
 
-    # for quote in quotes:
-    #     print(quote)
-    #     generate_image(out_folder, quote[1], quote[0])
+    print(len(quotes))
+
+    for quote in quotes:
+        print(quote)
+        generate_image(out_folder, quote[1], quote[0])
